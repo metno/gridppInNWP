4621abbed28422da710782954bd46704db9833ab
Trygve Aspelien <trygveasp@met.no>
2018-04-08 21:31:34 +0200

    ...

--- bufr/read_synop.py
+++ bufr/read_synop.py
@@ -2,18 +2,29 @@
 
 from __future__ import print_function
 import traceback
-import sys
+import argparse
 import numpy as np
-from cartopy import crs as ccrs
-import matplotlib.pyplot as plt
- 
-from eccodes import *
+CAN_PLOT=True
+try:
+    from cartopy import crs as ccrs
+    import matplotlib.pyplot as plt
+except:
+    CAN_PLOT=False
 
-VERBOSE=False
+import sys
+from eccodes import codes_set,codes_bufr_new_from_file,CodesInternalError,codes_release,codes_get,CODES_MISSING_DOUBLE,CODES_MISSING_LONG
 
-def example(INPUT):
+
+class observation():
+    def __init__(self, lon, lat, value, elevation):
+        self.lon = lon
+        self.lat = lat
+        self.value = value
+        self.elevation = elevation
+
+def readBufrFile(bufrFile,var,lonrange,latrange):
     # open bufr file
-    f = open(INPUT)
+    f = open(bufrFile)
  
     # define the keys to be printed
     keys = [
@@ -21,10 +32,12 @@ def example(INPUT):
         #'stationNumber',
         'latitude',
         'longitude',
-        'airTemperatureAt2M',
-        'relativeHumidity',
-        'totalSnowDepth'
+        'heightOfStation'
     ]
+    #'airTemperatureAt2M',
+    #    'relativeHumidity',
+    #    'totalSnowDepth'
+    keys.append(var)
  
     # The cloud information is stored in several blocks in the
     # SYNOP message and the same key means a different thing in different
@@ -34,15 +47,15 @@ def example(INPUT):
     # We know that this is the first occurrence of the keys we want to
     # read so in the list above we used the # (occurrence) operator
     # accordingly.
- 
-    cnt = 0
 
-    lons=list()
-    lats=list()
-    t2m=list()
-    sd=list()
-    rh2m=list() 
+
+    print("Reading "+bufrFile)
+    print("Looking for keys: "+str(keys))
+    cnt = 0
+    observations=list()
+
     # loop for the messages in the file
+    not_found = 0
     while 1:
         # get handle for message
         bufr = codes_bufr_new_from_file(f)
@@ -59,95 +72,125 @@ def example(INPUT):
         
         lat=np.nan
         lon=np.nan
-        t=np.nan
-        r=np.nan
-        s=np.nan
+        value=np.nan
+        elev=np.nan
         for key in keys:
             try:
                 val=codes_get(bufr, key)
                 #if val != CODES_MISSING_DOUBLE:
                 #    print('  %s: %s' % (key,val))
-                if val == CODES_MISSING_DOUBLE: val=np.nan
+                if val == CODES_MISSING_DOUBLE or val == CODES_MISSING_LONG: val=np.nan
                 if key == "latitude": lat=val
                 if key == "longitude": lon=val
-                if key == "airTemperatureAt2M": t=val
-                if key == "relativeHumidity": r=val
-                if key == "totalSnowDepth": s=val
+                if key == "heightOfStation": elev=val
+                if key == var:
+                    value = val
+                    if var == "relativeHumidity":
+                        if value > 100: values = 100.
+                    elif var == "airTemperatureAt2M":
+                        value=value-273.15
+
             except CodesInternalError as err:
-                dummy=1
-                print('Error with key="%s" : %s' % (key, err.msg))
- 
-        lats.append(lat)
-        lons.append(lon)
-        t2m.append(t)
-        rh2m.append(r)
-        sd.append(s)
+                if key == var:
+                    not_found=not_found+1
+                #print('Error with key="%s" : %s' % (key, err.msg))
+
+        if lat > latrange[0] and lat < latrange[1] and lon > lonrange[0] and lon < lonrange[1]:
+            if not np.isnan(value):
+                observations.append(observation(lon,lat,value,elev))
 
         cnt += 1
  
         # delete handle
         codes_release(bufr)
- 
+
+    print("Found "+str(len(observations))+"/"+str(cnt))
+    print("Not encoded for "+str(var)+": "+str(not_found))
     # close the file
     f.close()
- 
-    print(len(lons))
-    print(len(lats))
-    print(len(t2m))
-    print(len(sd))
-    print(len(rh2m))
+    return observations
 
-    proj = ccrs.LambertConformal(central_longitude=15., central_latitude=63., standard_parallels=[63.])
-    # Accent, Accent_r, Blues, Blues_r, BrBG, BrBG_r, BuGn, BuGn_r, BuPu, BuPu_r, CMRmap, CMRmap_r, Dark2, Dark2_r,
-    # GnBu, GnBu_r, Greens, Greens_r, Greys, Greys_r, OrRd, OrRd_r, Oranges, Oranges_r, PRGn, PRGn_r, Paired, Paired_r,
-    # Pastel1, Pastel1_r, Pastel2, Pastel2_r, PiYG, PiYG_r, PuBu, PuBuGn, PuBuGn_r, PuBu_r, PuOr, PuOr_r, PuRd, PuRd_r,
-    # Purples, Purples_r, RdBu, RdBu_r, RdGy, RdGy_r, RdPu, RdPu_r, RdYlBu, RdYlBu_r, RdYlGn, RdYlGn_r, Reds, Reds_r,
-    # Set1, Set1_r, Set2, Set2_r, Set3, Set3_r, Spectral, Spectral_r, Wistia, Wistia_r, YlGn, YlGnBu, YlGnBu_r, YlGn_r,
-    # YlOrBr, YlOrBr_r, YlOrRd, YlOrRd_r, afmhot, afmhot_r, autumn, autumn_r, binary, binary_r, bone, bone_r, brg,
-    # brg_r, bwr, bwr_r, cool, cool_r, coolwarm, coolwarm_r, copper, copper_r, cubehelix, cubehelix_r, flag, flag_r,
-    # gist_earth, gist_earth_r, gist_gray, gist_gray_r, gist_heat, gist_heat_r, gist_ncar, gist_ncar_r, gist_rainbow,
-    # gist_rainbow_r, gist_stern, gist_stern_r, gist_yarg, gist_yarg_r, gnuplot, gnuplot2, gnuplot2_r, gnuplot_r, gray,
-    # gray_r, hot, hot_r, hsv, hsv_r, inferno, inferno_r, jet, jet_r, magma, magma_r, nipy_spectral, nipy_spectral_r,
-    # ocean, ocean_r, pink, pink_r, plasma, plasma_r, prism, prism_r, rainbow, rainbow_r, seismic, seismic_r, spectral,
-    # spectral_r, spring, spring_r, summer, summer_r, terrain, terrain_r, viridis, viridis_r, winter, winter_r
-    plt.close()
-    ax = plt.axes(projection=proj)
-    ax.coastlines(resolution="10m")
-    print(t2m)
-    print(rh2m)
-    for t in range(0,len(rh2m)):
-        if rh2m[t] > 100: rh2m[t]=100.
-    print(rh2m)
-    print(sd)
-    plt.scatter(lons, lats, c=t2m, transform=ccrs.PlateCarree(),cmap="Paired")
-    plt.colorbar()
-    plt.show()
-    plt.close()
-    ax = plt.axes(projection=proj)
-    ax.coastlines(resolution="10m")
-    plt.scatter(lons, lats, c=rh2m, transform=ccrs.PlateCarree(),cmap="Paired")
-    plt.colorbar()
-    plt.show()
-    plt.close()
-    ax = plt.axes(projection=proj)
-    ax.coastlines(resolution="10m")
-    plt.scatter(lons, lats, c=sd, transform=ccrs.PlateCarree(),cmap="Paired")
-    plt.colorbar()
-    plt.show()
+def writeObs(fname,observations):
 
-def main(INPUT):
+    fh=open(fname,"w")
+    fh.write("lon;lat;elev;value\n")
+    for obs in observations:
+        fh.write(str(obs.lat)+";"+str(obs.lon)+";"+str(obs.elevation)+";"+str(obs.value)+"\n")
+    fh.close()
+
+
+def two_floats(value):
+    values = value.split(",")
+    #print(values)
+    if len(values) != 2:
+        raise argparse.ArgumentError
+    values = map(float, values)
+    return values
+
+def main(argv):
+
+    parser = argparse.ArgumentParser(description='Parse airTemperatureAt2M,relativeHumidity or totalSnowDepth from bufr file')
+    parser.add_argument('-f',dest="input",help='Inputfile file', required=True)
+    parser.add_argument('-o', dest="output", help='Output file', required=True)
+    parser.add_argument('-v',dest="var",help='variabel',required=True)
+    parser.add_argument('--plot',dest="plot",default=False,action='store_true')
+    parser.add_argument('-latrange', help='Only keep stations within these latitudes (min,max)', dest="latrange", action = 'store', type=two_floats,default=[-180,180])
+    parser.add_argument('-lonrange', help='Only keep stations within these longitudes (min,max)',dest="lonrange", action = 'store', type=two_floats,default=[-180,180])
+    parser.add_argument('--debug', dest="debug", help='Debug', action='store')
+
+    if len(argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    args = parser.parse_args()
+
     try:
-        example(INPUT)
+        observations=readBufrFile(args.input,args.var,args.lonrange,args.latrange)
     except CodesInternalError as err:
-        if VERBOSE:
+        if args.debug:
             traceback.print_exc(file=sys.stderr)
         else:
             sys.stderr.write(err.msg + '\n')
  
         return 1
- 
- 
+
+
+    writeObs(args.output,observations)
+
+    if args.plot and CAN_PLOT:
+        lons=list()
+        lats=list()
+        values=list()
+        elevs=list()
+        for obs in observations:
+            lats.append(obs.lat)
+            lons.append(obs.lon)
+            values.append(obs.value)
+            elevs.append(obs.elevation)
+
+        proj = ccrs.LambertConformal(central_longitude=15., central_latitude=63., standard_parallels=[63.])
+        # Accent, Accent_r, Blues, Blues_r, BrBG, BrBG_r, BuGn, BuGn_r, BuPu, BuPu_r, CMRmap, CMRmap_r, Dark2, Dark2_r,
+        # GnBu, GnBu_r, Greens, Greens_r, Greys, Greys_r, OrRd, OrRd_r, Oranges, Oranges_r, PRGn, PRGn_r, Paired, Paired_r,
+        # Pastel1, Pastel1_r, Pastel2, Pastel2_r, PiYG, PiYG_r, PuBu, PuBuGn, PuBuGn_r, PuBu_r, PuOr, PuOr_r, PuRd, PuRd_r,
+        # Purples, Purples_r, RdBu, RdBu_r, RdGy, RdGy_r, RdPu, RdPu_r, RdYlBu, RdYlBu_r, RdYlGn, RdYlGn_r, Reds, Reds_r,
+        # Set1, Set1_r, Set2, Set2_r, Set3, Set3_r, Spectral, Spectral_r, Wistia, Wistia_r, YlGn, YlGnBu, YlGnBu_r, YlGn_r,
+        # YlOrBr, YlOrBr_r, YlOrRd, YlOrRd_r, afmhot, afmhot_r, autumn, autumn_r, binary, binary_r, bone, bone_r, brg,
+        # brg_r, bwr, bwr_r, cool, cool_r, coolwarm, coolwarm_r, copper, copper_r, cubehelix, cubehelix_r, flag, flag_r,
+        # gist_earth, gist_earth_r, gist_gray, gist_gray_r, gist_heat, gist_heat_r, gist_ncar, gist_ncar_r, gist_rainbow,
+        # gist_rainbow_r, gist_stern, gist_stern_r, gist_yarg, gist_yarg_r, gnuplot, gnuplot2, gnuplot2_r, gnuplot_r, gray,
+        # gray_r, hot, hot_r, hsv, hsv_r, inferno, inferno_r, jet, jet_r, magma, magma_r, nipy_spectral, nipy_spectral_r,
+        # ocean, ocean_r, pink, pink_r, plasma, plasma_r, prism, prism_r, rainbow, rainbow_r, seismic, seismic_r, spectral,
+        # spectral_r, spring, spring_r, summer, summer_r, terrain, terrain_r, viridis, viridis_r, winter, winter_r
+        plt.close()
+        ax = plt.axes(projection=proj)
+        ax.coastlines(resolution="10m")
+        plt.scatter(lons, lats, c=values, transform=ccrs.PlateCarree(),cmap="coolwarm")
+        plt.title(args.var)
+        plt.colorbar()
+        plt.show()
+        plt.close()
+
 if __name__ == "__main__":
-    sys.exit(main(sys.argv[1]))
+    sys.exit(main(sys.argv))
 
 
